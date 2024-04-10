64adec3f237e459bc61c3a539372330c94ffa22a
J Vonk <s3340740@vuw.leidenuniv.nl>
2024-04-10 19:13:32 +0200

    improve

--- Kamerleden-Aanwezig.py
+++ Kamerleden-Aanwezig.py
@@ -1,117 +1,113 @@
-from cgi import print_form
 import sys
-from numpy import dtype
 import requests as req
 import xml.etree.ElementTree as ET
 import json
 from datetime import date
 
-# Get most recent 'vergaderverslag' from tweedekamer API
-def getFile():
-  today = date.today()
-  # 'vergaderverslag' of today will only be published very late today or early tomorrow
-  year = today.year
-  month = today.month
-  day = today.day - 1
-  url = f"https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/Verslag?$filter=year(GewijzigdOp)%20eq%20{year}%20and%20month(GewijzigdOp)%20eq%20{month}%20and%20day(GewijzigdOp)%20eq%20{day}"
-  r = req.get(url)
-  return r.content
 
-# Get vergaderID from json
-def vergaderID(content):
-  val = json.loads(content)
-  return val["value"][0]["Id"]
-  
-# Get 'Vergaderverslag'
-def getVerslag(vergID):
-  url = f"https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/Verslag/{vergID}/resource"
-  print(url)
-  r = req.get(url)
-  return r.content
+# Fetches the most recent 'vergaderverslag' from tweedekamer API
+def get_file():
+    today = date.today()
+    # 'vergaderverslag' of today will only be published very late today or early tomorrow
+    year, month, day = today.year, today.month, today.day - 1
+    url = f"https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/Verslag?$filter=year(GewijzigdOp)%20eq%20{year}%20and%20month(GewijzigdOp)%20eq%20{month}%20and%20day(GewijzigdOp)%20eq%20{day}"
+    response = req.get(url)
+    return response.content
 
-# Parse the XML received from the API
-def parseXML(verslag):
-  next = False
-  kamerleden = ""
-  verslag = verslag.decode()
 
-  try:
-    root = ET.fromstring(verslag)
-  except:
-    raise Exception("Error parsing XML")
+# Extracts 'Vergaderverslag' ID from JSON
+def extract_vergader_id(content):
+    data = json.loads(content)
+    return data["value"][0]["Id"]
 
-  # Parse XML and extract specific element
-  ns = {'ns': 'http://www.tweedekamer.nl/ggm/vergaderverslag/v1.0'}
-  alinea_elements  = root.findall(".//ns:alineaitem", namespaces=ns)
-  for alinea in alinea_elements:
-    if next:
-      kamerleden = alinea.text
-      break
-    if "leden der Kamer, te weten:" in str(alinea.text):
-      next = True
-  
-  # Format and transform into array
-  kamerleden = kamerleden.lower().replace(" en ",",").replace(" ","").split(",")
-  # Last index is invalid ez fix
-  return kamerleden[:len(kamerleden)-1]
 
-# Match names from present list to total list
-def stringSimilarity(target, source, matched):
-  consistent = 0
-  for i in range(len(source)):
-    if source[i] in matched:
-      continue
-    consistent = 0
-    for j in range(len(target)):
+# Fetches 'Vergaderverslag' content
+def get_verslag(vergader_id):
+    url = f"https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/Verslag/{vergader_id}/resource"
+    response = req.get(url)
+    return response.content
 
-      # If the length of one of the strings has been reached or if enough letters are matched
-      if j >= len(source[i]) or j >= len(target) or consistent >= len(source[i]) - 1:
-        # If enough letters are matched accept it
-        if consistent >= len(source[i]) - 1:
-          matched.append(source[i])
-          # print(f"mathced {target} to {source[i]}")
-          return True
-        else: break
 
-      # Compare letters
-      if target[j] == source[i][j]:
-        consistent += 1
-  # No match found
-  return False
+# Parses the XML received from the API
+def parse_xml(verslag):
+    next_flag = False
+    kamerleden = ""
 
-# Checking presence
-def presentie(aanwezig):
-  matched = []
-  integer = 0
-  # Open file with all members
-  f = open("files/2dekmrledn.txt", 'r')
-  print("----Afwezig:----")
+    try:
+        root = ET.fromstring(verslag.decode())
+    except ET.ParseError:
+        raise Exception("Error parsing XML")
 
-  # Check who are present at vergaderingen and mark in 'matched' array
-  for line in f:
-    if stringSimilarity(line.rstrip('\n'), aanwezig, matched):
-      integer += 1
-    else:
-      print(line.rstrip('\n'))
-  
-  # Not everyone has been matched
-  if integer is not len(aanwezig):
-    raise Exception(f"Aantal Kamerleden matcht niet met het aanwezige aantal is {integer} maar moet zijn {len(aanwezig)}")
+    # Parse XML and extract specific element
+    ns = {"ns": "http://www.tweedekamer.nl/ggm/vergaderverslag/v1.0"}
+    alinea_elements = root.findall(".//ns:alineaitem", namespaces=ns)
+    for alinea in alinea_elements:
+        if next_flag:
+            kamerleden = alinea.text
+            break
+        if "leden der Kamer, te weten:" in str(alinea.text):
+            next_flag = True
 
-  print(integer, "/", len(aanwezig))
-  return aanwezig
-    
-# Make nice graph who is present and who is not
-def makeGraph(aanwezig):
-  pass
+    # Format and transform into array
+    kamerleden = kamerleden.lower().replace(" en ", ",").replace(" ", "").split(",")
+    # Last index is invalid, removing it
+    return kamerleden[:-1]
+
+
+# Matches names from present list to total list
+def string_similarity(target, source, matched):
+    for i, src in enumerate(source):
+        if src in matched:
+            continue
+        for j, tgt in enumerate(target):
+            if j >= len(src) or j >= len(tgt):
+                if len(matched) == len(source) - 1:
+                    matched.append(src)
+                    return True
+                else:
+                    break
+            if tgt[j] == src[j] and j == len(src) - 1:
+                matched.append(src)
+                return True
+    return False
+
+
+# Checks presence
+def check_presence(present_list):
+    matched = []
+    integer = 0
+    # Open file with all members
+    with open("files/2dekmrledn.txt", "r") as f:
+        print("----Afwezig:----")
+        # Check who are present at vergaderingen and mark in 'matched' array
+        for line in f:
+            if string_similarity(line.rstrip("\n"), present_list, matched):
+                integer += 1
+            else:
+                print(line.rstrip("\n"))
+    # Not everyone has been matched
+    if integer != len(present_list):
+        raise Exception(
+            f"Aantal Kamerleden matcht niet met het aanwezige aantal: {integer}, maar moet zijn {len(present_list)}"
+        )
+
+    print(integer, "/", len(present_list))
+    return present_list
+
+
+# Makes a graph indicating who is present and who is not
+def make_graph(present_list):
+    pass
+
 
 def main():
-  content = getFile()
-  vergID = vergaderID(content)
-  verslag = getVerslag(vergID)
-  kamerleden = parseXML(verslag)
-  aanwezig = presentie(kamerleden)
-  makeGraph(aanwezig)
+    content = get_file()
+    vergader_id = extract_vergader_id(content)
+    verslag = get_verslag(vergader_id)
+    kamerleden = parse_xml(verslag)
+    aanwezig = check_presence(kamerleden)
+    make_graph(aanwezig)
 
-if __name__=="__main__":
-  main()
+
+if __name__ == "__main__":
+    main()
