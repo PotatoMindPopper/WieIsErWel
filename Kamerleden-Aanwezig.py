3ab6542ff87ec2ecb0c306f9675fb3a484aa1090
J.G.P. Vonk <j.g.p.vonk@umail.leidenuniv.nl>
2024-04-11 12:28:32 +0200

    try to use numpy as that was the intention of the original code

--- Kamerleden-Aanwezig.py
+++ Kamerleden-Aanwezig.py
@@ -614,23 +614,32 @@ def create_chart(attendance_list):
 
     :param attendance_list: Een lijst van Kamerleden die aanwezig waren
     """
-    import matplotlib.pyplot as plt
+    # import matplotlib.pyplot as plt
 
     labels = ["Afwezig", "Aanwezig"]
     sizes = [150 - len(attendance_list), len(attendance_list)]
     colors = ["red", "green"]
     explode = (0.1, 0)  # explode 1st slice
 
-    plt.pie(
-        sizes,
-        explode=explode,
-        labels=labels,
-        colors=colors,
-        autopct="%1.1f%%",
-        shadow=True,
-        startangle=140,
-    )
-    plt.axis("equal")
+    # plt.pie(
+    #     sizes,
+    #     explode=explode,
+    #     labels=labels,
+    #     colors=colors,
+    #     autopct="%1.1f%%",
+    #     shadow=True,
+    #     startangle=140,
+    # )
+    # plt.axis("equal")
+    # plt.show()
+    
+    # Gebruik numpy om de grafiek te maken
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    _, ax = plt.subplots()
+    ax.pie(sizes, explode=explode, labels=labels, colors=colors, autopct="%1.1f%%", shadow=True, startangle=140)
+    ax.axis("equal")
     plt.show()
 
 
