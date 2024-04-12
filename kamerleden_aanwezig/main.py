5250f98f0e5d2720e7c50771158fd4eec8999833
J.G.P. Vonk <j.g.p.vonk@umail.leidenuniv.nl>
2024-04-12 13:05:04 +0200

    improve printing per vergaderverslag

--- kamerleden_aanwezig/main.py
+++ kamerleden_aanwezig/main.py
@@ -8,6 +8,7 @@ from kamerleden_aanwezig.fetch_reports import fetch_reports
 from kamerleden_aanwezig.parse_xml import parse_xml
 from kamerleden_aanwezig.check_attendance import check_attendance
 from kamerleden_aanwezig.create_chart import create_chart
+from colorama import Fore, Style
 
 
 def main():
@@ -20,11 +21,15 @@ def main():
     if not reports:
         print("[main()] Geen 'vergaderverslag' gevonden.")
         return
+    header_length = 38 + 35 + 30 + 18 + 17 + 5 * 2
     for report in reports:
         try:
-            print("[main()] ================================") # TODO: Print datum of id of iets dergelijks
+            # print("[main()] ================================") # TODO: Print datum of id of iets dergelijks
+            print(f"{Fore.BLACK}{Style.BRIGHT}{'-' * header_length}{Style.RESET_ALL}")
+            print(f"{Fore.BLACK}{Style.BRIGHT}|{Style.RESET_ALL}{Style.BRIGHT}{f'Vergaderverslag: {report[2]}':^{header_length-2}}{Fore.BLACK}|{Style.RESET_ALL}") # TODO: Print datum of id of iets dergelijks
+            # print(f"{Fore.BLACK}{Style.BRIGHT}{'-' * header_length}{Style.RESET_ALL}")
             kamerleden = parse_xml(report)
-            attendance = check_attendance(kamerleden)
+            attendance = check_attendance(kamerleden, report[2])
             create_chart(attendance)
         except Exception as e:
             print(f"[main()] Er is een fout opgetreden: {e}")
