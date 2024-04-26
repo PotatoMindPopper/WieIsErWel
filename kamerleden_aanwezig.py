94030a6a7c15b854793bdff7859764a2aab4a9ab
J.G.P. Vonk <j.g.p.vonk@umail.leidenuniv.nl>
2024-04-26 11:36:37 +0200

    Improve pieces of code (styling and complexity)

--- kamerleden_aanwezig.py
+++ kamerleden_aanwezig.py
@@ -3,6 +3,7 @@ import logging
 from datetime import date, datetime, timedelta
 import os
 import requests
+import time
 import xml.etree.ElementTree as ET
 import difflib
 import numpy
@@ -16,20 +17,21 @@ class PresentieError(Exception):
 
 
 # Make a request to a given url (tries again, when connection errors occur)
-def make_request(url):
-  try:
-    response = requests.get(url)
-  except requests.exceptions.ConnectionError as e:
-    logging.info(f"[make_request()] ConnectionError occured: {e}")
-    try_again = 10
-    while response.status_code != 200 and try_again > 0:
-      try:
-        response = requests.get(url)
-      except requests.exceptions.ConnectionError as e:
-        logging.info(f"[make_request()] ConnectionError occured: {e}")
-        try_again -= 1
-        continue
-  return response
+def make_request(url, max_retries=3, retry_delay=2):
+  for attempt in range(max_retries):
+    try:
+      response = requests.get(url)
+      if response.status_code == 200:
+        return response
+    except requests.exceptions.ConnectionError as e:
+      logging.error(f"[make_request()] Connection error occurred: {e}")
+    logging.info(f"[make_request()] Retrying... ({attempt+1}/{max_retries})")
+    time.sleep(retry_delay)
+  logging.error(
+    "[make_request()] Failed to retrieve data from "
+    + f"{url} after {max_retries} attempts."
+  )
+  return None
 
 
 # Get the 'Vergaderverslagen' information from the Tweede Kamer API for a given
@@ -52,71 +54,77 @@ def get_url_content(datum):
     + "%20and%20"
     + f"day(GewijzigdOp)%20eq%20{datum.day}"
   )
-
   logging.debug(f"[get_url_content()] URL: {url}")
 
   response = make_request(url)
-  return response.json()
+  if response and response.status_code == 200:
+    return response.json()
+  else:
+    logging.error(f"[get_url_content()] Failed to fetch data from URL: {url}")
+    return None
 
 
 # Get vergader id(s) from JSON content
 def get_vergader_ids(content):
-  vergader_ids = []
-  for line in content["value"]:
-    if not line["Verwijderd"]:
-      logging.debug(f"[get_vergader_ids()] Line: {line}")
-      vergader_ids.append(line["Id"])
-  return vergader_ids
-  
+  vergader_ids = [
+    line["Id"] for line in content["value"] if not line.get("Verwijderd", False)
+  ]
+  logging.debug(f"[get_vergader_ids()] Vergader IDs: {vergader_ids}")
+  return vergader_ids  
+
 
 # Get 'Vergaderverslagen' for the given list of vergader id(s)
 def get_verslagen(vergader_ids):
   verslagen = []
   for vergader_id in vergader_ids:
-    url = f"https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0/Verslag/{vergader_id}/resource"
-    logging.debug(f"[get_verslagen()] URL: {url}")
-    verslagen.append(make_request(url))
+    url = (
+      "https://gegevensmagazijn.tweedekamer.nl/OData/v4/2.0"
+      + f"/Verslag/{vergader_id}/resource"
+    )
+    logging.debug(f"[get_verslagen()] Fetching data from URL: {url}")
+    response = make_request(url)
+    if response:
+      verslagen.append(response.json())
+    else:
+      logging.error(f"[get_verslagen()] Failed to fetch data from URL: {url}")
   return verslagen
 
 
 # Determines the latest verslag among list of 'Vergaderverslagen'
 def determine_latest_verslag(verslagen):
-  default_time = "0001-01-01T00:00:00.0000000+00:00"
+  default_time = datetime.min
+  latest_verslag_index = -1
+  max_time = default_time
 
-  timestamps = []
-  for verslag in verslagen:
+  for i, verslag in enumerate(verslagen):
     try:
       root = ET.fromstring(verslag.content.decode())
     except Exception as e:
       raise PresentieError(f"Error parsing XML: {e}")
 
+    ns = {"ns": "http://www.tweedekamer.nl/ggm/vergaderverslag/v1.0"}
     if (
-        root[0][1].text != "Plenaire zaal"
-        or root.attrib["soort"] == "Voorpublicatie"
+        root.find("./ns:zaal", namespaces=ns) != "Plenaire zaal"
+        or root.get("soort", "") == "Voorpublicatie"
     ):
-      timestamps.append(datetime.fromisoformat(default_time))
       continue
 
     timestamp = root.get("Timestamp", default_time)
     timestamp = datetime.fromisoformat(timestamp)
     logging.debug(f"[latest_verslag()] Timestamp: {timestamp}")
-    timestamps.append(timestamp)
+    if timestamp > max_time:
+      max_time = timestamp
+      latest_verslag_index = i
 
-  max_time = datetime.fromisoformat(default_time)
-  max_element = -1
-  for i, tijd in enumerate(timestamps):
-    if tijd > max_time:
-      max_time = tijd
-      max_element = i
-
-  if max_element == -1:
+  if latest_verslag_index == -1:
     return -1
 
   logging.debug(
-    f"[latest_verslag()] Max element: {max_element}; Max time: {max_time}"
+    f"[latest_verslag()] Latest verslag index: {latest_verslag_index}; "
+    + f"Max time: {max_time}"
   )
 
-  latest_verslag = verslagen[max_element].content.decode()
+  latest_verslag = verslagen[latest_verslag_index].content.decode()
   return latest_verslag
 
 
@@ -135,29 +143,25 @@ def parse_xml(verslagen):
   ns = {"ns": "http://www.tweedekamer.nl/ggm/vergaderverslag/v1.0"}
   alinea_elements = root.findall(".//ns:alineaitem", namespaces=ns)
 
-  next_alinea = False
   kamerleden = None
   for alinea in alinea_elements:
-    if next_alinea:
-      kamerleden = alinea.text
-      logging.debug(f"[parse_xml()] Attending members: {kamerleden}")
-      break
-    if "leden der Kamer, te weten:" in str(alinea.text):
+    if "leden der Kamer, te weten:" in alinea.text:
       # TODO: Check if kamerleden are present in this text, instead of next one
-      # Set the flag to check the next alinea for attending members
-      next_alinea = True
+      kamerleden = alinea.findnext(
+        "{http://www.tweedekamer.nl/ggm/vergaderverslag/v1.0}alineaitem"
+      )
+      if kamerleden is not None:
+        kamerleden = kamerleden.text
+        logging.debug(f"[parse_xml()] Attending members: {kamerleden}")
+      break
 
   if kamerleden is None:
     return -1
 
-  kamerleden = (
-    kamerleden
-      .lower()
-      .rstrip(",")
-      .replace(" en ", ",")
-      .replace(" ", "")
-      .split(",")
-  )
+  kamerleden = [
+    lid.strip() for lid in 
+    kamerleden.lower().rstrip(",").replace(" en ", ",").split(",")
+  ]
 
   logging.debug(
     f"[parse_xml()] Type of attending members: {type(kamerleden)}; "
@@ -171,7 +175,6 @@ def parse_xml(verslagen):
 def presentie(aanwezig):
   matched = []
   afwezig = []
-  integer = 0
 
   print("----Afwezig:----")
   with open("files/2dekmrledn.txt", "r") as f:
@@ -187,22 +190,22 @@ def presentie(aanwezig):
       )
       if close_matches:
         matched.append(close_matches[0])
-        integer += 1
       else:
         print(line.rstrip("\n"))
         afwezig.append(line.rstrip("\n"))
 
   print(
-    f"{integer} / {len(aanwezig)} kamerleden aanwezig, {len(afwezig)} afwezigen"
+    f"{len(matched)} / {len(aanwezig)} kamerleden aanwezig, "
+    + f"{len(afwezig)} afwezigen"
   )
 
   # Check if everyone has been matched
-  if integer != len(aanwezig):
+  if len(matched) != len(aanwezig):
     print(
       "Aantal Kamerleden matcht niet met het aantal aanwezige: "
-      + f"is {integer}, maar moet zijn {len(aanwezig)}"
+      + f"is {len(matched)}, maar moet zijn {len(aanwezig)}"
     )
-    unmatched = [member for member in aanwezig if member not in matched]
+    unmatched = set(aanwezig) - set(matched)
     print(f"De volgende leden konden niet worden gematched: {unmatched}")
 
   logging.debug(f"[presentie()] Afwezig: {afwezig}")
@@ -257,6 +260,7 @@ def aanwezigheid(datum):
     raise PresentieError(f"{datum}({type(datum)}) is not a valid date")
 
   # TODO: Add a waiting text, until presentie(kamerleden) gets called
+  print("Processing...")
 
   # Fetch the content of the meeting report for the specified date
   content = get_url_content(datum)
@@ -265,10 +269,10 @@ def aanwezigheid(datum):
   vergader_ids = get_vergader_ids(content)
 
   # If no meeting ID is found, return None for both present and absent members
-  if len(vergader_ids) == 0:
+  if not vergader_ids:
     return None, None
 
-  logging.debug(f"{vergader_ids[0]}")
+  logging.debug(f"[aanwezigheid()] Meeting ID: {vergader_ids[0]}")
 
   # Fetch the meeting reports based on the meeting IDs
   verslagen = get_verslagen(vergader_ids)
@@ -286,46 +290,57 @@ def aanwezigheid(datum):
   # Write present and absent members to a log file
   with open(f"files/logs/log{datum}.txt", "a") as f:
     f.write("Aanwezig:\n")
-    for string in aanwezig:
-      f.write(string + "\n")
+    f.write("\n".join(aanwezig) + "\n")
     f.write("\nAfwezig:\n")
-    for string in afwezig:
-      f.write(string + "\n")
+    f.write("\n".join(afwezig) + "\n")
 
   # Return the lists of present and absent members
   return aanwezig, afwezig
 
 
 def process_single_date():
-  datum = date.fromisoformat(input("Geef een datum op (YYYY-MM-DD): "))
+  datum_str = input("Geef een datum op (YYYY-MM-DD): ")
+  try:
+    datum = date.fromisoformat(datum_str)
+  except ValueError:
+    print("Ongeldige datumformaat. Voer de datum in het formaat YYYY-MM-DD in.")
+    return [], []
+
   aanwezig, afwezig = aanwezigheid(datum)
   aanwezig_arr, afwezig_arr = [], []
-  if aanwezig and afwezig:
+  if aanwezig is not None and afwezig is not None:
     aanwezig_arr.extend(aanwezig)
     afwezig_arr.extend(afwezig)
   return aanwezig_arr, afwezig_arr
 
 
 def process_multi_date():
-  datum1 = date.fromisoformat(input("Geef een eerste datum op (YYYY-MM-DD): "))
-  datum2 = date.fromisoformat(input("Geef een tweede datum op (YYYY-MM-DD): "))
-  delta = datum2 - datum1
+  datum1_str = input("Geef een eerste datum op (YYYY-MM-DD): ")
+  datum2_str = input("Geef een tweede datum op (YYYY-MM-DD): ")
+  try:
+    datum1 = date.fromisoformat(datum1_str)
+    datum2 = date.fromisoformat(datum2_str)
+  except ValueError:
+    print("Ongeldige datumformaat. Voer de datum in het formaat YYYY-MM-DD in.")
+    return [], []
+
+  delta = (datum2 - datum1).days
 
   aanwezig_arr, afwezig_arr = [], []
-  for _ in range(delta.days):
-    datum1 += timedelta(days=1)
-    if datum1.isoweekday() in (6, 7):
+  for i in range(1, delta):
+    datum = datum1 + timedelta(days=i)
+    if datum.isoweekday() in (6, 7):
       continue
 
-    if datum1 == date.today():
+    if datum == date.today():
       print(
         "Verslag van vandaag is er waarschijnlijk niet, "
         + "dus deze wordt niet gezocht"
       )
       continue
 
-    aanwezig, afwezig = aanwezigheid(datum1)
-    if aanwezig and afwezig:
+    aanwezig, afwezig = aanwezigheid(datum)
+    if aanwezig is not None and afwezig is not None:
       aanwezig_arr.extend(aanwezig)
       afwezig_arr.extend(afwezig)
   return aanwezig_arr, afwezig_arr
@@ -355,17 +370,21 @@ def process_graph(aanwezig_arr, afwezig_arr):
 
 def main():
   aanwezig_arr, afwezig_arr = process_dates()
-  process_graph(aanwezig_arr, afwezig_arr)
+  if aanwezig_arr or afwezig_arr:
+    process_graph(aanwezig_arr, afwezig_arr)
+  else:
+    print("Geen aanwezigheidsgegevens beschikbaar om een grafiek te maken.")
 
 
 if __name__ == "__main__":
   # Parse command-line arguments
-  parser = argparse.ArgumentParser(description="Set debug mode for printing")
+  parser = argparse.ArgumentParser(
+    description="Process presence status and create a graph."
+  )
   parser.add_argument(
     "--debug",
     action="store_true",
-    help="Set debug to True if you want to see the output of "
-    + "the getting and parsing process",
+    help="Enable debug mode to print detailed output during the process.",
   )
   args = parser.parse_args()
 
